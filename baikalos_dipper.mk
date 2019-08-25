d23e46041dd4bd15cdd8f9b9efde8ca0ac3e2c9b
sdv22 <sdv22@live.ru>
2019-08-25 09:53:58 +0300

    Import fingerprint from LaOS

--- baikalos_dipper.mk
+++ baikalos_dipper.mk
@@ -20,14 +20,14 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:9/PKQ1.180729.001/V10.3.2.0.PEAMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEACNFA release-keys" \
+    PRIVATE_BUILD_DESC="dipper-user 9 PKQ1.180729.001 V10.3.2.0.PEAMIXM release-keys" \
     PRODUCT_NAME="dipper" \
     TARGET_DEVICE="dipper"
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # BaikalOS Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
